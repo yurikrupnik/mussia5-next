7f71c9d348d1946a5db7c918b2ec27410c4cfc8e
yurikrupnik <krupnik.yuri@gmail.com>
2021-01-30 10:06:22 +0200

    fixed deepscan

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -5,50 +5,53 @@ import connectDb from "../../middlewares/db";
 
 // connectDb();
 function onError(err, req, res, next) {
-  console.log(err);
+    console.log(err);
 
-  res.status(500).end(err.toString());
-  // OR: you may want to continue
-  next();
+    res.status(500).end(err.toString());
+    // OR: you may want to continue
+    next();
 }
 
 function onNoMatch(req, res) {
-  console.log("req", req);
-  res.status(404).end("page is not found... or is it");
+    console.log("req", req);
+    res.status(404).end("page is not found... or is it");
 }
 
-handler.use((req, res, next) => {
-  req.hello = "world";
-  // call next if you want to proceed to next in chain
-  next();
-});
-// Or include a base
+// todo eslint did not catch this = DeepScan did
+// https://deepscan.io/dashboard/#view=project&tid=12695&pid=15729&bid=318942&prid=&subview=issues&status=%5B%22open%22%2C%22confirmed%22%5D&page=1&impact=%5B%22High%22%2C%22Medium%22%2C%22Low%22%5D
+
+// handler.use((req, res, next) => {
+//   req.hello = "world";
+//   // call next if you want to proceed to next in chain
+//   next();
+// });
+// // Or include a base
 // handler.use("/foo", fn); // Only run in /foo/**
 
 // Mount an instance of next-connect
 // const common = nc().use(midd1).use(midd2); // You may have some common middleware to be used in every route.
 // const auth = nc().use("/dashboard", checkAuth);
 const subapp = nc()
-  .get((req, res) => {
-    res.statusCode = 200;
-    res.send("add");
-  })
-  .get("/baz", onNoMatch);
+    .get((req, res) => {
+        res.statusCode = 200;
+        res.send("add");
+    })
+    .get("/baz", onNoMatch);
 
 const handler = nc({ onError, onNoMatch }).use(morgan("dev")).use(connectDb);
 
 handler
-  // .use(common) // `midd1` and `midd2` runs everywhere
-  // .use(auth) // `checkAuth` runs on /dashboard/*
-  .use("/foo", subapp); // `getHandle` runs on /foo while `postHandle` runs on /foo/baz
+    // .use(common) // `midd1` and `midd2` runs everywhere
+    // .use(auth) // `checkAuth` runs on /dashboard/*
+    .use("/foo", subapp); // `getHandle` runs on /foo while `postHandle` runs on /foo/baz
 
 const base = nc({ onError, onNoMatch })
-  .use(morgan("dev"))
-  .use(connectDb)
-  .get((req, res) => {
-    // res.statusCode(200)
-    res.send("aris");
-  });
+    .use(morgan("dev"))
+    .use(connectDb)
+    .get((req, res) => {
+        // res.statusCode(200)
+        res.send("aris");
+    });
 
 // base.get("/base", (req, res) => {
 //   // res.statusCode(200)
@@ -63,21 +66,21 @@ const base = nc({ onError, onNoMatch })
 // handler.use(connectDb);
 
 handler.get((req, res) => {
-  res.statusCode = 200;
-  res.json({ name: "Arisss" });
+    res.statusCode = 200;
+    res.json({ name: "Arisss" });
 });
 
 handler.get("/a", base);
 
 handler.use("/base", (req, res) => {
-  res.statusCode = 200;
-  res.json({ name: "base" });
-  // res.statusCode(200).send("ok base");
+    res.statusCode = 200;
+    res.json({ name: "base" });
+    // res.statusCode(200).send("ok base");
 });
 
 handler.post((req, res) => {
-  res.statusCode = 200;
-  res.json({ name: "posting" });
+    res.statusCode = 200;
+    res.json({ name: "posting" });
 });
 
 export default handler;
