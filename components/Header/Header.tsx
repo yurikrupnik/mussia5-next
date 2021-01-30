cfaa2102d08bbae82c8004d66e74b5d21511b5e2
yurikrupnik <krupnik.yuri@gmail.com>
2021-01-30 23:55:15 +0200

    added tsts

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { fade, makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
@@ -7,6 +7,14 @@ import Typography from "@material-ui/core/Typography";
 import InputBase from "@material-ui/core/InputBase";
 import Badge from "@material-ui/core/Badge";
 import MenuItem from "@material-ui/core/MenuItem";
+// import Drawer from "@material-ui/core/Drawer";
+// import Hidden from "@material-ui/core/Hidden";
+// import List from "@material-ui/core/List";
+// import ListItem from "@material-ui/core/ListItem";
+// import ListItemIcon from "@material-ui/core/ListItemIcon";
+// import ListItemText from "@material-ui/core/ListItemText";
+// import Divider from "@material-ui/core/Divider";
+// import InboxIcon from "@material-ui/icons/MoveToInbox";
 import Menu from "@material-ui/core/Menu";
 import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
@@ -14,72 +22,45 @@ import AccountCircle from "@material-ui/icons/AccountCircle";
 import MailIcon from "@material-ui/icons/Mail";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import MoreIcon from "@material-ui/icons/MoreVert";
+// import { useSession } from "next-auth/client";
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
-    grow: {
-        flexGrow: 1,
+    root: {
+        display: "flex",
+    },
+    drawer: {
+        [theme.breakpoints.up("sm")]: {
+            // width: drawerWidth,
+            // flexShrink: 0,
+        },
+    },
+    appBar: {
+        [theme.breakpoints.up("sm")]: {
+            width: `calc(100% - ${drawerWidth}px)`,
+            marginLeft: drawerWidth,
+        },
     },
     menuButton: {
         marginRight: theme.spacing(2),
-    },
-    title: {
-        display: "none",
         [theme.breakpoints.up("sm")]: {
-            display: "block",
-        },
-    },
-    search: {
-        position: "relative",
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: fade(theme.palette.common.white, 0.15),
-        "&:hover": {
-            backgroundColor: fade(theme.palette.common.white, 0.25),
-        },
-        marginRight: theme.spacing(2),
-        marginLeft: 0,
-        width: "100%",
-        [theme.breakpoints.up("sm")]: {
-            marginLeft: theme.spacing(3),
-            width: "auto",
-        },
-    },
-    searchIcon: {
-        padding: theme.spacing(0, 2),
-        height: "100%",
-        position: "absolute",
-        pointerEvents: "none",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    inputRoot: {
-        color: "inherit",
-    },
-    inputInput: {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-        transition: theme.transitions.create("width"),
-        width: "100%",
-        [theme.breakpoints.up("md")]: {
-            width: "20ch",
-        },
-    },
-    sectionDesktop: {
-        display: "none",
-        [theme.breakpoints.up("md")]: {
-            display: "flex",
-        },
-    },
-    sectionMobile: {
-        display: "flex",
-        [theme.breakpoints.up("md")]: {
             display: "none",
         },
+    },
+    // necessary for content to be below app bar
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+        // width: drawerWidth,
+    },
+    content: {
+        // flexGrow: 1,
+        // padding: theme.spacing(3),
     },
 }));
 
 export default function PrimarySearchAppBar() {
+    // const [session] = useSession();
     const classes = useStyles();
     const [anchorEl, setAnchorEl] = React.useState(null);
     const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
@@ -103,7 +84,38 @@ export default function PrimarySearchAppBar() {
     const handleMobileMenuOpen = () => {
         // setMobileMoreAnchorEl(event.currentTarget);
     };
+    // const [mobileOpen, setMobileOpen] = React.useState(false);
 
+    // const handleDrawerToggle = () => {
+    //     setMobileOpen(!mobileOpen);
+    // };
+
+    // const drawer = (
+    //     <div>
+    //         <div className={classes.toolbar} />
+    //         <Divider />
+    //         <List>
+    //             {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+    //                 <ListItem button key={text}>
+    //                     <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+    //                     <ListItemText primary={text} />
+    //                 </ListItem>
+    //             ))}
+    //         </List>
+    //         <Divider />
+    //         <List>
+    //             {["All mail", "Trash", "Spam"].map((text, index) => (
+    //                 <ListItem button key={text}>
+    //                     <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+    //                     <ListItemText primary={text} />
+    //                 </ListItem>
+    //             ))}
+    //         </List>
+    //     </div>
+    // );
+    // const img = useMemo(() => (session && session.user && session.user.image) || "", [session]);
+
+    // console.log("img", img);
     const menuId = "primary-search-account-menu";
     const renderMenu = (
         <Menu
@@ -131,7 +143,6 @@ export default function PrimarySearchAppBar() {
             open={isMobileMenuOpen}
             onClose={handleMobileMenuClose}
         >
-            <div>as</div>
             <MenuItem>
                 <IconButton aria-label="show 4 new mails" color="inherit">
                     <Badge badgeContent={4} color="secondary">
@@ -163,30 +174,32 @@ export default function PrimarySearchAppBar() {
     );
 
     return (
-        <div className={classes.grow}>
+        <>
             <AppBar position="static">
                 <Toolbar>
                     <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="open drawer">
                         <MenuIcon />
                     </IconButton>
-                    <Typography className={classes.title} variant="h6" noWrap>
+                    <Typography variant="h6" noWrap>
                         Mussia
                     </Typography>
-                    <div className={classes.search}>
-                        <div className={classes.searchIcon}>
+                    <div>
+                        <div>
                             <SearchIcon />
                         </div>
                         <InputBase
                             placeholder="Search…"
-                            classes={{
-                                root: classes.inputRoot,
-                                input: classes.inputInput,
-                            }}
+                            classes={
+                                {
+                                    // root: classes.inputRoot,
+                                    // input: classes.inputInput,
+                                }
+                            }
                             inputProps={{ "aria-label": "search" }}
                         />
                     </div>
-                    <div className={classes.grow} />
-                    <div className={classes.sectionDesktop}>
+                    <div />
+                    <div>
                         <IconButton aria-label="show 4 new mails" color="inherit">
                             <Badge badgeContent={4} color="secondary">
                                 <MailIcon />
@@ -208,7 +221,7 @@ export default function PrimarySearchAppBar() {
                             <AccountCircle />
                         </IconButton>
                     </div>
-                    <div className={classes.sectionMobile}>
+                    <div>
                         <IconButton
                             aria-label="show more"
                             aria-controls={mobileMenuId}
@@ -221,8 +234,9 @@ export default function PrimarySearchAppBar() {
                     </div>
                 </Toolbar>
             </AppBar>
+
             {renderMobileMenu}
             {renderMenu}
-        </div>
+        </>
     );
 }
