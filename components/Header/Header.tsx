e7c30b75fee975ce436fb84d62065eade5d030f5
yurikrupnik <krupnik.yuri@gmail.com>
2021-01-30 20:07:07 +0200

    fixed deepscan 1

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { SyntheticEvent } from "react";
+import React from "react";
 import { fade, makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
