2799383ad10b4525677960d23f46d7ea0f1790a5
yurikrupnik <krupnik.yuri@gmail.com>
2021-01-31 14:46:25 +0200

    fixed branch

--- middlewares/all/index.ts
+++ middlewares/all/index.ts
@@ -2,7 +2,7 @@ import nc from "next-connect";
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
-import passport from "@/middlewares/passport";
+// import passport from "@/middlewares/passport";
 import connectDb from "@/middlewares/db";
 import session from "@/middlewares/session";
 
@@ -12,16 +12,17 @@ const handler = nc()
     .use(cookieParser())
     .use(connectDb)
     .use(session)
-    .use(passport.initialize())
-    .use(passport.session())
+    // .use(passport.initialize())
+    // .use(passport.session())
     .use((req, res, next) => {
         console.log("re.sessions", req.sessions); // eslint-disable-line no-console
         console.log("re.user", req.user); // eslint-disable-line no-console
-        if (req.isAuthenticated()) {
-            console.log("Authenticated user"); // eslint-disable-line no-console
-        } else {
-            console.log("Not Authenticated user"); // eslint-disable-line no-console
-        }
+        console.log("re.user", req.cookie); // eslint-disable-line no-console
+        // if (req.isAuthenticated()) {
+        //     console.log("Authenticated user"); // eslint-disable-line no-console
+        // } else {
+        //     console.log("Not Authenticated user"); // eslint-disable-line no-console
+        // }
         next();
     });
 
--- utils/crypt.ts
+++ utils/crypt.ts
@@ -1,9 +1,10 @@
-import bcrypt from "bcrypt";
-
-const generateHash = (password: string) => bcrypt.hash(password, bcrypt.genSaltSync(10));
-
-const generateHashSync = (password: string) => bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-
-const validatePassword = (password: string, hash: string) => bcrypt.compare(password, hash);
-
-export { generateHash, validatePassword, generateHashSync };
+// import bcrypt from "bcrypt";
+//
+// const generateHash = (password: string) => bcrypt.hash(password, bcrypt.genSaltSync(10));
+//
+// const generateHashSync = (password: string) => bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+//
+// const validatePassword = (password: string, hash: string) => bcrypt.compare(password, hash);
+//
+// export { generateHash, validatePassword, generateHashSync };
+export default {};
