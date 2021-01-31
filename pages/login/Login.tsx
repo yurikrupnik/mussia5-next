a8740fb48258967b628745f7a66005aace5c53b2
yurikrupnik <krupnik.yuri@gmail.com>
2021-01-31 22:18:21 +0200

    removed build

--- pages/login/Login.tsx
+++ pages/login/Login.tsx
@@ -83,7 +83,7 @@ const Login = () => {
                     <img src={logo} alt="logo" />
                 </Hidden>
                 <Hidden mdUp>
-                    <img src={logoBlack} alt="logo" />
+                    <img src={logoBlack} alt="logos" />
                 </Hidden>
             </Grid>
             <Grid container item sm={10} xs={12} direction="row" justify="center" alignItems="center">
