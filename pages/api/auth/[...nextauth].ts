9d6f796aef5cbbb50026192ba97d425daa86d748
yurikrupnik <krupnik.yuri@gmail.com>
2021-02-09 19:27:26 +0200

    added duplicated button

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -26,20 +26,54 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default (req: NextApiRequest, res: NextApiResponse) =>
     NextAuth(req, res, {
         providers: [
+            // Providers.Credentials({
+            //     // The name to display on the sign in form (e.g. 'Sign in with...')
+            //     name: "Credentials",
+            //     // The credentials is used to generate a suitable form on the sign in page.
+            //     // You can specify whatever fields you are expecting to be submitted.
+            //     // e.g. domain, username, password, 2FA token, etc.
+            //     credentials: {
+            //         username: { label: "Username", type: "text", placeholder: "jsmith" },
+            //         password: { label: "Password", type: "password" },
+            //     },
+            //     async authorize(credentials) {
+            //         console.log("credentials", credentials);
+            //         // // Add logic here to look up the user from the credentials supplied
+            //         // const user = { id: 1, name: "J Smith", email: "jsmith@example.com" };
+            //         //
+            //         // if (user) {
+            //         //     // Any object returned will be saved in `user` property of the JWT
+            //         //     return user;
+            //         // }
+            //         // If you return null or false then the credentials will be rejected
+            //         return {};
+            //         // You can also Reject this callback with an Error or with a URL:
+            //         // throw new Error('error message') // Redirect to error page
+            //         // throw '/path/to/redirect'        // Redirect to a URL
+            //     },
+            // }),
             Providers.GitHub({
-                clientId: "",
-                clientSecret: "",
-                scope: "",
+                // clientId: "453146d4f22a9dc52f30",
+                clientId: "Iv1.5b44de52edb0b343",
+                clientSecret: "a98d15f9f52a5adde9643d2decd95ec8cdc46bd0",
+                // clientSecret: "e2652da3b0fa1320b294ba4b35d979ccf52aff69",
             }),
             Providers.Google({
                 // clientID: "552054097269-8dpq61pku540cl71mb9jtho3dag3sglk.apps.googleusercontent.com",
                 // clientSecret: process.env.GOOGLE_CLIENT_SECRET || "JG5rjZqAZluroM6eZ2H254LR",
-                clientId: "206012550014-pj6ilj273h60b7qiokg3dh4j450391er.apps.googleusercontent.com",
+                clientId: "206012550014-djactupct5k19rbem7m6da2kffni2vmj.apps.googleusercontent.com",
                 // clientId: process.env.GOOGLE_CLIENT_ID,
                 // clientSecret: process.env.GOOGLE_CLIENT_SECRET
-                clientSecret: "vJfuqmQAACdwAGGMCXtpdCQX",
+                clientSecret: "W1eaP-uDDtck8dgYG7TYQgRg",
             }),
+            Providers.Email({
+                from: "krupnik.yuri@gmail.com",
+                maxAge: 50000,
+                // server: "",
+            }),
+            // Providers.Credentials({}),
             // ...add more providers here
         ],
         database: "mongodb://0.0.0.0/mussia5-next",
+        // debug: true,
     });
--- pages/api/system/index.ts
+++ pages/api/system/index.ts
@@ -7,7 +7,7 @@ const handler = nc().use(all);
 handler.get((req: NextApiRequest, res: NextApiResponse) => {
     // res.statusCode = 200;
     // res.json({ ad: "yes" });
-    res.status(200).json({ ar: "s" });
+    res.status(200).json({ ar: "ssss" });
 });
 
 export default handler;
