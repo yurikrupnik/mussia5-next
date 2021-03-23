972860856894642c9be349fa7ca186da6eac0540
yurikrupnik <krupnik.yuri@gmail.com>
2021-03-24 00:04:18 +0200

    branch name added

--- pages/aris/aris.tsx
+++ pages/aris/aris.tsx
@@ -3,7 +3,7 @@ import classes from "./aris.module.scss";
 
 const Aris: React.FC = () => {
     const [yes] = useState(false);
-    return <div className={classes.root}>Aris {yes.toString()}</div>;
+    return <div className={classes.root}>Aris and more {yes.toString()}</div>;
 };
 
 export default Aris;
