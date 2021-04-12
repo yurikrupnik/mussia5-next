0338c27bea14a33678202d9972ed80ee5115b4db
yuri <yuri.krupnik@perimeter81.com>
2021-04-12 23:10:12 +0300

    fixed lint

--- utils/auth0.ts
+++ utils/auth0.ts
@@ -5,7 +5,7 @@ console.log("process.env.VERCEL_URL", process.env.VERCEL_URL); // eslint-disable
 export default initAuth0({
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    // // domain: "client-apps-dev.eu.auth0.com" || process.env.AUTH0_DOMAIN
+    // // domain: "client-apps-dev.eu.auth0.com" || process.env.AUTH0_DOMAINlp
     secret: process.env.AUTH0_SECRET,
     issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
     baseURL: process.env.VERCEL_URL,
