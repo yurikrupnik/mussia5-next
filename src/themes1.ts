75b08b9c0372ef2bb02f6bb637a499218a24873d
yuri <yuri.krupnik@perimeter81.com>
2021-04-12 22:17:16 +0300

    fixed lint

--- src/themes1.ts
+++ src/themes1.ts
@@ -3,7 +3,7 @@ import { createMuiTheme } from "@material-ui/core/styles";
 const theme = createMuiTheme({
     palette: {
         primary: {
-            main: "#8ad06c",
+            main: "#35691e",
         },
     },
 });
