3583e720f15b17b857072a6c7a38138f532a93a1
yuri <yuri.krupnik@perimeter81.com>
2021-04-15 05:34:58 +0300

    fixed lint

--- middlewares/db/index.ts
+++ middlewares/db/index.ts
@@ -3,8 +3,9 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const connection: Partial<{ isConnected: ConnectionStates }> = {};
 
-console.log("process.env.MONGODB_URIf", process.env.MONGODB_URI); // eslint-disable-line
 async function connectDb(req: NextApiRequest, res: NextApiResponse, next: () => void) {
+    console.log("process.env.MONGODB_URIf", process.env.MONGODB_URI); // eslint-disable-line
+    console.log("process.env.VERCEL_URL", process.env.VERCEL_URL); // eslint-disable-line
     if (connection.isConnected) {
         console.log("Using existing connection"); // eslint-disable-line
         return next();
