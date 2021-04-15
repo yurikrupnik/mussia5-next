4baad59ae92108c9ad25bb40eede36eadbddb720
yuri <yuri.krupnik@perimeter81.com>
2021-04-15 07:03:52 +0300

    removed baseUrl

--- utils/auth0.ts
+++ utils/auth0.ts
@@ -6,7 +6,8 @@ export default initAuth0({
     // // domain: "client-apps-dev.eu.auth0.com" || process.env.AUTH0_DOMAIN,
     secret: process.env.AUTH0_SECRET,
     issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
-    // baseURL: process.env.VERCEL_URL,
+
+    baseURL: process.env.AUTH0_BASE_URL,
     // // issuerBaseURL: "http://localhost:3000",;
     // session: {
     //     cookie: {
